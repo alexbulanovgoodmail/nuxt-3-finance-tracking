ea3d6247fdb682242a682deb2ba1b4c685bf4b44
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2024-01-08 13:50:09 +0300

    User Profile Nested Routes, Vertical Navigation and Settings Page

---
+++ pages/settings.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const links = [
+	{
+		label: 'Profile',
+		to: '/settings/profile'
+	},
+	{
+		label: 'Avatar',
+		to: '/settings/avatar'
+	},
+	{
+		label: 'Setting',
+		to: '/settings/settings'
+	}
+]
+</script>
+
+<template>
+	<div class="grid grid-cols-6 gap-8">
+		<UVerticalNavigation class="col-span-2" :links="links" />
+
+		<div class="col-span-4">
+			<NuxtPage />
+		</div>
+	</div>
+</template>
