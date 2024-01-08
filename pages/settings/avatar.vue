537178e1bf12da9bcf2d863826a3aa157a030b1b
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2024-01-08 18:11:05 +0300

    Uploading Files Avatar Image Actual Upload to Storage and Access Policies

--- pages/settings/avatar.vue
+++ pages/settings/avatar.vue
@@ -1,5 +1,97 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const { toastSuccess, toastError } = useAppToast()
+const uploading = ref<boolean>(false)
+const fileInput = ref<HTMLInputElement | null>(null)
+
+console.log(user.value)
+
+const saveAvatar = async () => {
+	const file = fileInput.value?.input.files[0]
+
+	if (!file) {
+		toastError({
+			title: 'Select a file to upload first'
+		})
+
+		return
+	}
+
+	const fileExt = file.name.split('.').pop()
+	const fileName = `${Math.random()}.${fileExt}`
+
+	try {
+		uploading.value = true
+		const currentAvatarUrl = user.value?.user_metadata?.avatar_url
+
+		console.log('currentAvatarUrl', currentAvatarUrl)
+
+		const { error } = await supabase.storage
+			.from('avatars')
+			.upload(fileName, file)
+
+		if (error) {
+			throw error
+		}
+
+		await supabase.auth.updateUser({
+			data: {
+				avatar_url: fileName
+			}
+		})
+
+		fileInput.value!.input.value = null
+
+		toastSuccess({
+			title: 'Avatar uploaded'
+		})
+	} catch (error: any) {
+		toastError({
+			title: 'Error uploading avatar',
+			description: error.message
+		})
+	} finally {
+		uploading.value = false
+	}
+}
+</script>
 
 <template>
-	<div>Avatar!</div>
+	<div>
+		<div class="mb-4">
+			<UFormGroup
+				label="Current avatar"
+				class="w-full"
+				help="This would be blank by default"
+			>
+				<UAvatar
+					src="https://avatars.githubusercontent.com/u/739984?v=4"
+					size="3xl"
+				/>
+			</UFormGroup>
+		</div>
+
+		<div class="mb-4">
+			<UFormGroup
+				label="New avatar"
+				class="w-full"
+				name="avatar"
+				help="After choosing an image click Save to actually upload the new avatar"
+			>
+				<UInput type="file" ref="fileInput" />
+			</UFormGroup>
+		</div>
+
+		<UButton
+			type="submit"
+			color="black"
+			variant="solid"
+			label="Save"
+			:loading="uploading"
+			:disabled="uploading"
+			@click="saveAvatar"
+		/>
+	</div>
 </template>
